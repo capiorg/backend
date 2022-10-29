ef758b041534b96e7fdcddfcde92a1cd250deb37
dotX12 <dev@shitposting.team>
2022-10-29 07:32:10 +0300

    fix cache setup

--- misc.py
+++ misc.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.db.asyncpg_utils import *  # noqa
-from config import settings_app, settings_redis
+from config import settings_app
+from config import settings_redis
 
 DATABASE_URL = settings_app.dsn
 
@@ -14,7 +15,8 @@ engine = create_async_engine(
     future=True,
     echo=False,
     connect_args={"timeout": 30},
-    pool_size=100,
+    pool_size=200,
+    max_overflow=300,
 )
 
 sync_engine = create_engine(settings_app.sync_dns)
@@ -29,10 +31,15 @@ autocommit_engine = engine.execution_options(isolation_level="AUTOCOMMIT")
 metadata = sqlalchemy.MetaData()
 Base = declarative_base(metadata=metadata)
 
-
 cache = Cache()
 cache.setup(
-    settings_redis.dsn,
+    settings_url=settings_redis.dsn(
+        host=settings_redis.REDIS_HOST,
+        port=settings_redis.REDIS_PORT,
+        user=settings_redis.REDIS_USER,
+        password=settings_redis.REDIS_PWD,
+        database=settings_redis.REDIS_DB,
+    ),
     client_side=True,
     retry_on_timeout=True,
     hash_key="1337",
